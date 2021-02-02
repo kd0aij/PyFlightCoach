e8df89a6419661d766d3b7101aab933750e3e9e8
Thomas David <thomasdavid0@gmail.com>
2021-02-02 17:48:45 +0000

    vertical 8 plot

--- create_elements.py
+++ create_elements.py
@@ -12,33 +12,42 @@ from geometry import Point, Quaternion
 
 
 st.markdown(
-        f"""
+    f"""
 <style>
     .reportview-container .main .block-container{{
         max-width: 90%;
     }}
 </style>
 """,
-        unsafe_allow_html=True,
-    )
+    unsafe_allow_html=True,
+)
 
-
-#initial = State(
-#            Point(30, 170, 150),
-#            Quaternion.from_euler(Point(0, 0, np.pi)),
-#            Point(30, 0, 0),
-#            Point(np.pi, 0, 0)
-#        )
 
 initial = State(
-            Point(0, 170, 150),
-            Quaternion.from_euler(Point(np.pi, 0, np.pi)),
-            Point(10 * np.pi, 0, 0), # 620 m in 10 seconds
-            Point(0, np.pi / 5, 0) # 
-        )
+    Point(20 * np.pi, 170, 150),
+    Quaternion.from_euler(Point(0, 0, np.pi)),
+    Point(10 * np.pi, 0, 0),
+    Point(np.pi / 2, 0, 0)
+)
 
-seq = Section.from_radius(initial, np.linspace(0, 10, 10))
+line = Section.from_line(initial, np.linspace(0, 2, 30))
 
+last_state = line.get_state_from_index(-1)
+last_state.brvel = Point(0, -np.pi / 5, 0)
+
+radius = Section.from_radius(last_state, np.linspace(0, 10, 50))
+
+last_state = radius.get_state_from_index(-1)
+last_state.brvel = Point(0, np.pi / 5, 0)
+
+radius2 = Section.from_radius(last_state, np.linspace(0, 10, 50))
+
+last_state = radius2.get_state_from_index(-1)
+last_state.brvel = Point(np.pi / 2, 0, 0)
+
+line2 = Section.from_line(last_state, np.linspace(0, 2, 30))
+
+seq = Section.stack([line, radius, radius2, line2])
 
 npoints = st.sidebar.number_input("Number of Models", 0, 50, value=20)
 scale = st.sidebar.number_input("Model Scale Factor", 1.0, 50.0, value=10.0)
@@ -46,9 +55,10 @@ showmesh = st.sidebar.checkbox("Show Models", False)
 cgtrace = st.sidebar.checkbox("Show CG Trace", True)
 ttrace = st.sidebar.checkbox("Show Tip Trace", False)
 
-
+ttot = float(seq.data.index[-1])
 plot_range = st.slider(
-    "plot range", 0.0, 10.0, (0.0, 10.0))
+    "plot range", 0.0, ttot, (0.0, ttot))
+
 
 def make_plot_data(seq, plot_range, npoints, showmesh, cgtrace, ttrace):
     sec = seq.subset(*plot_range)
@@ -60,8 +70,6 @@ def make_plot_data(seq, plot_range, npoints, showmesh, cgtrace, ttrace):
     if ttrace:
         traces += tiptrace(sec, scale * 1.85)
     return traces
-
-
 
 
 st.plotly_chart(
