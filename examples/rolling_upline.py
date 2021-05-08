60a820cacd9985b12a3c7b4abfdd95024f6f4f58
Mark Whitehorn <kd0aij@gmail.com>
2021-05-08 13:45:24 -0600

    add example html plots for slowroll and rolling_upline

--- examples/rolling_upline.py
+++ examples/rolling_upline.py
@@ -17,7 +17,7 @@ import tkinter as tk
 
 scale = 10
 obj = OBJ.from_obj_file('../data/models/ColdDraftF3APlane.obj').transform(Transformation(
-    Point(0.75, 0, 0), Quaternion.from_euler(Point(0, 0, np.pi/2))
+    Point(0.75, 0, 0), Quaternion.from_euler(Point(np.pi, 0, -np.pi/2))
 ))
 scaled_obj = obj.scale(scale * 1.85)
 duration = 32
@@ -25,7 +25,7 @@ duration = 32
 # Y center of aerobatic box is 150 meters from pilot box, 135 meters from runway centerline
 # far edge is 160 meters from runway centerline for a 50m total depth
 initPos = Point(0, 160, 10)
-initQ = Quaternion.from_euler(Point(np.pi, .1-np.pi/2, 0)) # noninverted
+initQ = Quaternion.from_euler(Point(np.pi, -np.pi/2, 0)) # inverted upline
 initVel = Point(600/32, 0, 0)
 initRates = Point(np.radians(360/duration), 0, 0)
 
