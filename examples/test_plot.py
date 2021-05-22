63949ef742e39f8079876d9a83b0342d26285c15
Mark Whitehorn <kd0aij@gmail.com>
2021-05-22 15:34:48 -0600

    fix color for plane models

--- examples/test_plot.py
+++ examples/test_plot.py
@@ -24,67 +24,68 @@ obj = OBJ.from_obj_file('/home/markw/linux_git/kd0aij/PyFlightCoach/data/models/
     )
 )
 
-# Thomas David's log
-FC_examples = '/mnt/c/Users/markw/GoogleDrive/blackbox_logs/FlightCoach/examples/logs/'
-binfile = '00000100'
-bin = Flight.from_log(FC_examples + binfile + '.BIN')
-origin = [51.4594504400,   -2.7912540674]
-# runway heading from Octave (degrees CW from North)
-heading = np.radians(-122)
-# If pilot north is flightline heading:
-# heading = wrapPi(heading + pi/2)
+# # Thomas David's log
+# FC_examples = '/mnt/c/Users/markw/GoogleDrive/blackbox_logs/FlightCoach/examples/logs/'
+# binfile = '00000100'
+# bin = Flight.from_log(FC_examples + binfile + '.BIN')
+# origin = [51.4594504400,   -2.7912540674]
+# # runway heading from Octave (degrees CW from North)
+# heading = np.radians(-122)
+# # If pilot north is flightline heading:
+# # heading = wrapPi(heading + pi/2)
+# box = Box(
+#         'heading',
+#         GPSPosition(
+#             origin[0],
+#             origin[1]
+#         ),
+#         heading
+#     )
+# box.to_json("TD_box.json")
+
+# start = 108
+# end = 154
+# start = 500
+# end = 600
+
+# AAM East Field: FlightLine.from_covariance heading is off by 180 degrees
+binfile = "P21_032521"
+# binfile = "M21_032521"
+bin = Flight.from_log('/home/markw/linux_git/kd0aij/PyFlightCoach/data/logs/' + binfile + ".BIN")
+# heading = np.radians(16)
+heading = np.radians(106)
+pilot_box = [39.842288, -105.212928]
 box = Box(
         'heading',
         GPSPosition(
-            origin[0],
-            origin[1]
+            pilot_box[0],
+            pilot_box[1]
         ),
         heading
     )
-box.to_json("TD_box.json")
+# box.to_json("AAMeast_box.json")
+
+
 
 print("Runway heading (CW from North) is {:5.1f} (East) / {:5.1f} (West)".format(
              np.degrees(heading), np.degrees(wrapPi(heading+pi))))
 
-start = 108
-end = 154
-
-# AAM East Field: FlightLine.from_covariance heading is off by 180 degrees
-#binfile = "P21_032521"
-# binfile = "M21_032521"
-# bin = Flight.from_log('/home/markw/linux_git/kd0aij/PyFlightCoach/data/logs/' + binfile + ".BIN")
-# heading = np.radians(16)
-# pilot_box = [39.842288, -105.212928]
-# box = Box(
-#         'heading',
-#         GPSPosition(
-#             pilot_box[0],
-#             pilot_box[1]
-#         ),
-#         heading
-#     )
-# box.to_json("AAMeast_box.json")
-
 flightline = FlightLine.from_box(box)
 sec = Section.from_flight(bin, flightline)
-
-# examples
-#binfile = "00000100"
-#binfile = "00000130"
-
-# bin = Flight.from_log('../../data/logs/' + binfile + ".BIN")
-# sec = Section.from_flight(bin, FlightLine.from_covariance(bin))
 
 # start = 275
 # end = 286
 # start = 60
 # end = 82
+start = 82
+end = 142
 
 subSec = sec.subset(start, end)
 
 mingspd = 10
 # TODO: lower pitch thresholds seem to be best for reducing errors in calculated maneuver roll
-pThresh = np.radians(30)
+# this seems to indicate that the ground course computing in genManeuverRPY is not correct.
+pThresh = np.radians(60)
 # TODO: why is flightline.transform_to the same as transform_from?
 [roll, pitch, wca, wca_axis] = genManeuverRPY(subSec, heading, mingspd, pThresh, flightline.transform_to)
 
@@ -92,15 +93,15 @@ span = 1.85
 scale = 5
 dt = end - start
 # draw models 1 second apart
-numModels = int(dt)
+numModels = 2*int(dt)
 
 # use orthographic projection for rendering to canvas
 fig = go.Figure(
         #boxfrustum() +
-        # boxfrustumEdges() +
+        boxfrustumEdges() +
         tiptrace(subSec, scale * span, roll, pitch, wca) +
         ribbon(scale * span * .9, subSec, roll) +
-        meshes(obj.scale(scale), numModels, subSec, 'orange', flightline.transform_to),
+        meshes(obj.scale(scale), numModels, subSec, roll),
         layout=go.Layout(
             margin=dict(l=0, r=0, t=1, b=0, autoexpand=True),
             legend=dict(
