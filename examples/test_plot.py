095d5745e9147ff7e5d569a200c6c696c306476d
Mark Whitehorn <kd0aij@gmail.com>
2021-06-02 07:53:16 -0600

    use box.json files in test_plot

--- examples/test_plot.py
+++ examples/test_plot.py
@@ -6,66 +6,77 @@ Created on Sun Apr  4 16:16:44 2021
 @author: markw
 """
 
-from flightanalysis import Section, FlightLine, State
+from flightanalysis import Section, FlightLine
 from flightanalysis.flightline import Box
 from flightdata import Flight
 import numpy as np
 from math import pi
-from flightplotting.traces import tiptrace, meshes, ribbon, boxtrace, \
-    boxfrustum, boxfrustumEdges, create_3d_plot, genManeuverRPY, wrapPi
+from flightplotting.traces import tiptrace, meshes, ribbon, \
+     boxfrustumEdges, genManeuverRPY, wrapPi
 from flightplotting.model import OBJ
-from geometry import Transformation, Point, Quaternion, Points, GPSPosition
+from geometry import Transformation, Point, Quaternion, GPSPosition
 import plotly.graph_objects as go
 
 obj = OBJ.from_obj_file('/home/markw/linux_git/kd0aij/PyFlightCoach/data/models/ColdDraftF3APlane.obj').transform(
     Transformation(
         Point(0.75, 0, 0),
-        Quaternion.from_euler(Point(np.pi, 0, -np.pi/2))
+        Quaternion.from_euler(Point(pi, 0, -pi/2))
     )
 )
 
-# Thomas David's log
-# FC_examples = '/mnt/c/Users/markw/GoogleDrive/blackbox_logs/FlightCoach/examples/logs/'
-# flight = Flight.from_log(FC_examples + logfile + '.BIN')
-# flight.to_csv('data/logs/flight_csvs/100.csv')
+testNum = 1
 
-# vertical 8
-# logfile = '100'
-# start = 108
-# end = 154
+# the heading in the box.json file must be a compass heading:
+# direction pilot is facing in degrees CW from true North
+if testNum == 0:
+    # Thomas David's logs
+    # FC_examples = '/mnt/c/Users/markw/GoogleDrive/blackbox_logs/FlightCoach/examples/logs/'
+    # flight = Flight.from_log(FC_examples + logfile + '.BIN')
+    # flight.to_csv('data/logs/flight_csvs/100.csv')
+    pbox = 'TD_box'
 
-logfile = '141'
-start = 221
-end = 242
+    # vertical 8
+    logfile = '100'
+    start = 108
+    end = 154
+
+elif testNum == 1:
+    pbox = 'TD_box'
+    # stall turn
+    logfile = '141'
+    start = 221
+    end = 242
+
+elif testNum == 2:
+    # AAM East Field: FlightLine.from_covariance heading is off by 180 degrees
+    logfile = "P21_032521"
+    # flight = Flight.from_log('/home/markw/linux_git/kd0aij/PyFlightCoach/data/logs/' + logfile + ".BIN")
+    # flight.to_csv('data/logs/flight_csvs/M21_032521.csv')
+
+    pbox = 'AAMeast_box'
+
+    # v8 and stall turn
+    start = 92
+    end = 146
+
+elif testNum == 3:
+    logfile = "M21_032521"
+    # pilot north is 16 degrees East of North
+    pbox = 'AAMeast_box'
+
+    # stall turn
+    start = 268
+    end = 288
 
 flight = Flight.from_csv('data/logs/flight_csvs/'+ logfile + '.csv')
 firstGPSpos = flight.origin()
 print("firstGPS position:", firstGPSpos)
 
-pilot_box = GPSPosition(51.4594504400, -2.7912540674)
-# pilot north is roughly SE
-heading = np.radians(148)
+box = Box.from_json('examples/' + pbox + '.json')
 
-# # AAM East Field: FlightLine.from_covariance heading is off by 180 degrees
-# logfile = "P21_032521"
-# # logfile = "M21_032521"
-# flight = Flight.from_log('/home/markw/linux_git/kd0aij/PyFlightCoach/data/logs/' + logfile + ".BIN")
-# # pilot north is 16 degrees East of North
-# heading = np.radians(16)
-# # heading = np.radians(106)
-# pilot_box = GPSPosition(39.842288, -105.212928)
+print("Pilot North (CW from North): {:5.1f}".format(np.degrees(box.heading)))
+print("pilot position: ", box.pilot_position)
 
-# # start = 275
-# # end = 286
-# # start = 60
-# # end = 82
-# start = 82
-# end = 142
-
-print("Pilot North (CW from North) is {:5.1f}".format(np.degrees(heading)))
-print("pilot position: ({:f}, {:f})".format(pilot_box.latitude, pilot_box.longitude))
-
-box = Box('heading', pilot_box, heading)
 flightline = FlightLine.from_box(box)
 sec = Section.from_flight(flight, flightline)
 
